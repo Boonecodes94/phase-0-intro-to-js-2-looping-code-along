32ace5471b9b26b00f895fd8b44ceedbc894d532
Heath Boone <hmboone1@gmail.com>
2023-07-06 18:57:23 -0500

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, surprise) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`);
+  }
+
+  return messages ;
+}
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+function countDown(startingNumber) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
